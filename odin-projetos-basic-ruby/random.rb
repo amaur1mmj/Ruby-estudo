93b6b5f516029e811822b09728f8360e1621e525
amaur1mmj <amaur1mmj@gmail.com>
2022-10-07 09:53:59 -0300

    OK, estou testando as mesnagens desse commit usando  o  nano.

--- odin-projetos-basic-ruby/random.rb
+++ odin-projetos-basic-ruby/random.rb
@@ -8,17 +8,19 @@ def ramdom_number(basic_array)
     for i in 0...basic_array.length
             ramdom = rand(0...basic_array.length) 
             temp = 0 
-            unless i == ramdom
+            if i != ramdom # pode ser usado com unless, porem tirando o elsif e com isso perdendo o poder de ramdomizar a posição quando os calores são iguais
                 temp = basic_array[i]
                 basic_array[i] = basic_array[ramdom]
                 basic_array[ramdom] = temp      
+            elsif i != 0
+                i -= 1
             end
 # algoritmo de ramdomização, fiz alguns testes mas não se ele ira quebrar com algua condição 
     end
     basic_array
 end
 
-for i in 0...5
+for i in 0...50
     p ramdom_number(array_integer)
-    p ramdom_number(array_string)
+    #p ramdom_number(array_string)
 end
--- odin-projetos-basic-ruby/substrings/FT-substring.rb
+++ odin-projetos-basic-ruby/substrings/FT-substring.rb
@@ -4,20 +4,7 @@ text = "Howdy sit below go going"
 def substrings(string, dictionary)
     p text_array = string.downcase.split(" ")
     bag_words = {}
-    cont = 0
-
-    for i in 0...text_array.length do
-        
-        for j in 0...dictionary.length do 
-            
-            if text_array[i] == dictionary[j]
-                bag_words[text_array[i]] = cont +=1        
-            end
-            
-        end
-        cont = 0 
-    end
-    bag_words
+    
 
 end
 
